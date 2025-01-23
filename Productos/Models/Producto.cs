bed621c0dd45d88553d7eb149ff7395791aec32b
Omar Soto <ojsg140789@gmail.com>
2025-01-23 16:05:40 -0600

    feature: Se agrega componente para listas productos

---
+++ Productos/Models/Producto.cs
@@ -0,0 +1,11 @@
+﻿namespace Productos.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Tipo { get; set; }
+        public decimal TasaInteres { get; set; }
+        public bool Status { get; set; }
+    }
+}
